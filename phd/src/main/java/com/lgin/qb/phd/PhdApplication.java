8054689b2c77b948889defefbf151c72d00c1a7b
doddarajesh <rajesh.dodda@letsgettin.com>
2019-06-03 15:44:57 +0530

    new project structure completed

---
+++ phd/src/main/java/com/lgin/qb/phd/PhdApplication.java
@@ -0,0 +1,13 @@
+package com.lgin.qb.phd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhdApplication.class, args);
+	}
+
+}
